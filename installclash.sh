b40208b955f4a956851f2128163455fd7ff30bf7
m0eak <zhongliangpsp@sina.com>
2022-07-03 17:48:25 +0800

    Upload

---
+++ installclash.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+opkg update
+
+opkg install dnsmasq-full --download-only && opkg remove dnsmasq && opkg install dnsmasq-full --cache . && rm *.ipk
+
+opkg install wget-ssl coreutils-nohup bash iptables curl ca-certificates ipset ip-full iptables-mod-tproxy iptables-mod-extra libcap libcap-bin ruby ruby-yaml kmod-tun luci-compat libcurl4
+
+wget -O /tmp/openclash.ipk https://github.com/vernesong/OpenClash/releases/download/v0.45.33-beta/luci-app-openclash_0.45.33-beta_all.ipk
+opkg install /tmp/openclash.ipk
+rm -f /tmp/openclash.ipk
